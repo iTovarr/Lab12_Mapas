57cb2bbd3bc94b73e68197f334134bb2c43c3246
iTovarr <13m.tovar.m@gmail.com>
2026-06-06 18:53:07 -0500

    feat: add multiple markers to MapScreen

--- app/src/main/java/com/example/lab12_maps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_maps/MapScreen.kt
@@ -3,9 +3,8 @@ package com.example.lab12_maps
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext // Importante para cargar recursos
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -13,24 +12,32 @@ import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
 
-
 @Composable
 fun MapScreen() {
-    val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
+    val ArequipaLocation = LatLng(-16.4040102, -71.559611)
     val cameraPositionState = rememberCameraPositionState {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
+
+    val locations = listOf(
+        LatLng(-16.433415, -71.5442652), // JLByR
+        LatLng(-16.4205151, -71.4945209), // Paucarpata
+        LatLng(-16.3524187, -71.5675994)  // Zamacola
+    )
 
     Box(modifier = Modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            Marker(
-                state = rememberMarkerState(position = ArequipaLocation),
-                title = "Arequipa, Perú",
-                icon = BitmapDescriptorFactory.fromResource(R.drawable.icon)
-            )
+            locations.forEach { location ->
+                Marker(
+                    state = rememberMarkerState(position = location),
+                    title = "Ubicación",
+                    snippet = "Punto de interés",
+                    icon = BitmapDescriptorFactory.fromResource(R.drawable.icon)
+                )
+            }
         }
     }
 }
