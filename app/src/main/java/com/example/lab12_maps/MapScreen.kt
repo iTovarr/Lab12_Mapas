1702f6ceb11dc0979509b619ebd9581adaec1f6b
iTovarr <13m.tovar.m@gmail.com>
2026-06-06 18:58:02 -0500

    feat: implement programmatic camera animation

--- app/src/main/java/com/example/lab12_maps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_maps/MapScreen.kt
@@ -3,8 +3,10 @@ package com.example.lab12_maps
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext // Importante para cargar recursos
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -19,17 +21,30 @@ fun MapScreen() {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
+    LaunchedEffect(Unit) {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(
+                LatLng(-16.2520984, -71.6836503), // Yura
+                12f
+            ),
+            durationMs = 3000
+        )
+    }
+
+    // Lista de ubicaciones
     val locations = listOf(
         LatLng(-16.433415, -71.5442652), // JLByR
         LatLng(-16.4205151, -71.4945209), // Paucarpata
         LatLng(-16.3524187, -71.5675994)  // Zamacola
     )
 
+    // Un solo contenedor y un solo mapa
     Box(modifier = Modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
+            // Todos los marcadores dentro del mismo bloque de GoogleMap
             locations.forEach { location ->
                 Marker(
                     state = rememberMarkerState(position = location),
