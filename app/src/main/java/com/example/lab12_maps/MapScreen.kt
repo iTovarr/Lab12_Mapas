c7908566cb352b7b1b7cd582d615f9c5d3e8ec02
iTovarr <13m.tovar.m@gmail.com>
2026-06-06 18:50:41 -0500

    feat: implement Google Maps and add custom marker icon

--- app/src/main/java/com/example/lab12_maps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_maps/MapScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
@@ -20,17 +21,15 @@ fun MapScreen() {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
-
     Box(modifier = Modifier.fillMaxSize()) {
-        // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
-                title = "Arequipa, Perú"
+                title = "Arequipa, Perú",
+                icon = BitmapDescriptorFactory.fromResource(R.drawable.icon)
             )
         }
     }
